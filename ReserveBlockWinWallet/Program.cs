9570dfdd283a0275ce0d97a9a91139c8d737b859
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-02-21 19:04:32 -0600

    Update Program.cs

--- ReserveBlockWinWallet/Program.cs
+++ ReserveBlockWinWallet/Program.cs
@@ -11,7 +11,7 @@ namespace ReserveBlockWinWallet
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
-            
+            //Build Compile
 
             ApplicationConfiguration.Initialize();
             Application.Run(new SplashScreenForm());
